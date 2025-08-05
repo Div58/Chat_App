43ce3f197191ad51518633b3b36c85b669a1300e
Div58 <144905795+Div58@users.noreply.github.com>
2025-08-06 01:20:15 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "https://chat-app-backend-d6le.onrender.com",
 })
 console.log('Axios Base URL:', axiosInstance.defaults.baseURL)
 console.log('import.meta.env.VITE_API_URL:', import.meta.env.VITE_API_URL)
